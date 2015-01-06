c0f11ebed52e91ee729b390ef4b2394364a546b5
swethakolluru <swetha.kolluru@valuelabs.net>
2015-01-06 18:01:29 +0530

    code modifications

--- src/GqAus/UserBundle/Services/UserService.php
+++ src/GqAus/UserBundle/Services/UserService.php
@@ -108,15 +108,92 @@ class UserService
         }//if
         return array('message' => $message, 'validRequest' => $validRequest);
     }
-	
-	/**
+    
+    /**
      * function to update course condition status.
      *  @return array
      */
-	public function updateCourseConditionStatus($user)
-	{
-		$user->setCourseConditionStatus('1');
-		$this->em->persist($user);
-		$this->em->flush();
-	}
+    public function updateCourseConditionStatus($user, $file)
+    {
+        $user->setCourseConditionStatus('1');
+        $this->em->persist($user);
+        $this->em->flush();
+        
+        ignore_user_abort(true);
+        $path = "../template/"; // change the path to fit your websites document structure
+        $dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\].]|[\.]{2,})", '', $file); // simple file name validation
+        $dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); // Remove (more) invalid characters
+        $fullPath = $path.$dl_file;
+
+        if ($fd = fopen ($fullPath, "r")) {
+            $fsize = filesize($fullPath);
+            $path_parts = pathinfo($fullPath);
+            $ext = strtolower($path_parts["extension"]);
+            switch ($ext) {
+                case "pdf":
+                header("Content-type: application/pdf");
+                header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
+                break;
+                // add more headers for other content types here
+                default;
+                header("Content-type: application/octet-stream");
+                header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+                break;
+            }
+            header("Content-length: $fsize");
+            header("Cache-control: private"); //use this to open files directly
+            while(!feof($fd)) {
+                $buffer = fread($fd, 2048);
+                echo $buffer;
+            }
+        }
+        fclose ($fd);
+    }
+    
+    /**
+     * function to get dashboard information.
+     *  @return array
+     */
+    public function getDashboardInfo($user)
+    {
+        $currentIdPoints = 100;
+        $maximumPoints = 100;
+        $profileCompleteness = 0;
+        if(is_object($user) && count($user) > 0) {
+           $userId = $user->getId();
+           $firstName = $user->getFirstName();
+           $lastName = $user->getLastName();
+           $email = $user->getEmail();
+           $phone = $user->getPhone();
+           $userImage = $user->getUserImage();
+           $address = $user->getAddress();
+           $address = count($address);
+           if (!empty($firstName)) {
+                $profileCompleteness += 15;
+           }
+           if (!empty($lastName)) {
+                $profileCompleteness += 15;
+           }
+           if (!empty($email)) {
+                $profileCompleteness += 15;
+           }
+           if (!empty($phone)) {
+                $profileCompleteness += 15;
+           }
+           if (!empty($address)) {
+                $profileCompleteness += 40;
+           }
+           if (empty($userImage)) {
+                $userImage = 'profielicon.png';
+           }
+           $percentage = ($profileCompleteness*$maximumPoints)/100;
+           $percentage = $percentage.'%';
+           $userCourses = $user->getCourses();
+           $courseConditionStatus = $user->getCourseConditionStatus();
+           return array('profileCompleteness' => $percentage, 
+                        'userImage' => $userImage,                                                                            'currentIdPoints' => $currentIdPoints,
+                        'userCourses' => $userCourses,
+                        'courseConditionStatus' => $courseConditionStatus);
+        }
+    }
 }
