3d2de90904c83bf5ad7c767e8a0bfbc6e11386d0
prashanthvanama <prashanthvanama@github.com>
2015-01-22 10:34:14 +0530

    added entities for Rto and added chage title code

--- src/GqAus/UserBundle/Controller/EvidenceController.php
+++ src/GqAus/UserBundle/Controller/EvidenceController.php
@@ -9,6 +9,9 @@ use GqAus\UserBundle\Form\EvidenceForm;
 class EvidenceController extends Controller
 {
     
+    /**
+    * Function to add the Evidence
+    */
     public function addAction(Request $request)
     {        
         $form = $this->createForm(new EvidenceForm(), array());
@@ -23,6 +26,9 @@ class EvidenceController extends Controller
         }
     }
     
+    /**
+    * Function to display all the Evidences
+    */
     public function viewAction(Request $request)
     { 
         $evidenceService = $this->get('EvidenceService');
@@ -33,7 +39,9 @@ class EvidenceController extends Controller
         );
     }
     
-    
+    /**
+    * Function to save the existing Evidence
+    */
     public function saveExistingEvidenceAction(Request $request)
     {
         if ($request->isMethod('POST')) {
@@ -43,13 +51,31 @@ class EvidenceController extends Controller
         }
     }
     
-     public function deleteEvidenceFileAction()
+    /**
+    * Function to delete Evidence file
+    */
+    public function deleteFileAction()
     {
         $evidenceId = $this->getRequest()->get('fid');
         $evidenceType = $this->getRequest()->get('ftype');
         $fileName = $this->get('EvidenceService')->deleteEvidence($evidenceId, $evidenceType);
         if ($evidenceType != 'text') {
             $this->get('gq_aus_user.file_uploader')->delete($fileName);
+        }
+        exit;
+    }
+    
+    /**
+    * Function to update Evidence Title
+    */
+    public function editTitleAction(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $title = $request->get('title');
+            $id = $request->get('id');
+            $evidenceService = $this->get('EvidenceService');
+            $evidenceService->updateEvidence($id,$title);
+            echo "success";
         }
         exit;
     }
---
+++ src/GqAus/UserBundle/Entity/Rto.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace GqAus\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rto
+ */
+class Rto extends User
+{ 
+    /**
+     * Role type for Rto
+     * 
+     * @var Integer
+     */
+    const ROLE = 4;
+    
+    /**
+     * @var string
+     */
+    const ROLE_NAME = 'ROLE_RTO';
+    
+    /**
+     * Get type
+     *
+     * @return integer 
+     */
+    public function getRole()
+    {
+        return self::ROLE;
+    }
+    
+    public function getRoleName()
+    {
+        return self::ROLE_NAME;
+    }
+
+}
