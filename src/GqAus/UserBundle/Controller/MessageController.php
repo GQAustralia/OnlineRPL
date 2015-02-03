7b9980e16982c8eb4758c8850978d97d668c5ddf
prashanthvanama <prashanthvanama@github.com>
2015-02-03 20:50:09 +0530

    message issues fixed

--- src/GqAus/UserBundle/Controller/MessageController.php
+++ src/GqAus/UserBundle/Controller/MessageController.php
@@ -9,10 +9,11 @@ use \DateTime;
 
 class MessageController extends Controller
 {
+
     /**
-    * Function to view all inbox messages
-    * return response
-    */
+     * Function to view all inbox messages
+     * return response
+     */
     public function viewAction(Request $request)
     {
         $messageService = $this->get('UserService');
@@ -24,13 +25,13 @@ class MessageController extends Controller
         $now = new DateTime('now');
         $result['today'] = $now->format('Y-m-d');
         return $this->render(
-                'GqAusUserBundle:Message:view.html.twig',$result);
+                        'GqAusUserBundle:Message:view.html.twig', $result);
     }
-    
+
     /**
-    * Function to save the message
-    * return response
-    */
+     * Function to save the message
+     * return response
+     */
     public function composeAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
@@ -39,7 +40,7 @@ class MessageController extends Controller
         $unreadcount = $userService->getUnreadMessagesCount($curuser->getId());
         $composeform = $this->createForm(new ComposeMessageForm(), array());
         /* Compose Action Start */
-        if ($request->isMethod('POST')) {            
+        if ($request->isMethod('POST')) {
             $composeform->handleRequest($request);
             if ($composeform->isValid()) {
                 $composearr = $request->get('compose');
@@ -66,6 +67,7 @@ class MessageController extends Controller
                 }
             }
         }
+        $newMsg = "true";
         $repMessage = $repSub = $repuser = '';
         $coursename = $request->get("course-name");
         $coursecode = $request->get("course-code");
@@ -78,32 +80,36 @@ class MessageController extends Controller
             $repSub = $coursename . $unitname;
             $repMessage = "Course details : " . $coursecode . " : " . $coursename . "\n";
             $repMessage .= "Unit details : " . $unitcode . " : " . $unitname;
+            $newMsg = "false";
         }
         $replyId = $this->getRequest()->get('reply_id');
         if ($replyId) {
             $message = $userService->getMessage($replyId);
             $repMessage = "\n\n\n\n";
-            $repMessage .= "Received on :" . $message->getCreated() . ", sent from : ".$message->getSent()->getUserName()."\n";
-            $repMessage .= "Subject :" . $message->getmessage() . "\n";
+            $repMessage .= "Received on :" . $message->getCreated() . ", sent from : " . $message->getSent()->getUserName() . "\n";
+            $repMessage .= "Subject :" . $message->getSubject() . "\n";
             $repMessage .= "Message :\n" . $message->getmessage();
             $repSub = "Re: " . $message->getSubject();
             $repuser = $message->getSent()->getEmail();
+            $newMsg = "false";
         }
         /* Compose Action End */
         return $this->render(
-                    'GqAusUserBundle:Message:compose.html.twig',
-                    array('composemsgForm' => $composeform->createView(),
+                        'GqAusUserBundle:Message:compose.html.twig', array(
+                    'composemsgForm' => $composeform->createView(),
                     'unreadcount' => $unreadcount,
                     'repMessage' => $repMessage,
                     'sub' => $repSub,
-                    'user' => $repuser)
+                    'user' => $repuser,
+                    'newMsg' => $newMsg
+                        )
         );
     }
-    
+
     /**
-    * Function to view all sent messages
-    * return response
-    */
+     * Function to view all sent messages
+     * return response
+     */
     public function sentAction(Request $request)
     {
         $messageService = $this->get('UserService');
@@ -116,13 +122,12 @@ class MessageController extends Controller
         $result['today'] = $now->format('Y-m-d');
         return $this->render('GqAusUserBundle:Message:sent.html.twig', $result);
     }
-    
+
     /**
-    * Function to view all trashed messages
-    * return response
-    */
-    public function trashAction(Request $request)
-    {
+     * Function to view all trashed messages
+     * return response
+     */
+    public function trashAction(Request $request) {
         $messageService = $this->get('UserService');
         $userid = $messageService->getCurrentUser()->getId();
         $unreadcount = $messageService->getUnreadMessagesCount($userid);
@@ -134,7 +139,7 @@ class MessageController extends Controller
         $result['today'] = $now->format('Y-m-d');
         return $this->render('GqAusUserBundle:Message:trash.html.twig', $result);
     }
-    
+
     public function draftAction(Request $request)
     {
         $messageService = $this->get('UserService');
@@ -142,41 +147,43 @@ class MessageController extends Controller
         $userid = $messageService->getCurrentUser()->getId();
         $unreadcount = $messageService->getUnreadMessagesCount($userid);
         return $this->render(
-            'GqAusUserBundle:Message:draft.html.twig',array('unreadcount' => $unreadcount));
+                        'GqAusUserBundle:Message:draft.html.twig', array('unreadcount' => $unreadcount));
     }
-    
+
     /**
-    * Function to mark as read / unread
-    */
+     * Function to mark as read / unread
+     */
     public function markAsReadAction(Request $request)
     {
         $readStatus = $request->get("readStatus");
         $checkedMessages = json_decode(stripslashes($request->get("checkedMessages")));
         $messageService = $this->get('UserService');
         $userid = $messageService->getCurrentUser()->getId();
-        foreach($checkedMessages as $cm) {
+        foreach ($checkedMessages as $cm) {
             $messageService->markReadStatus($cm, $readStatus);
         }
-        echo $messageService->getUnreadMessagesCount($userid)."&&success"; exit;
+        echo $messageService->getUnreadMessagesCount($userid) . "&&success";
+        exit;
     }
-    
+
     /**
-    * Function to trash messages.
-    */
+     * Function to trash messages.
+     */
     public function deleteFromUserAction(Request $request)
     {
         $type = $request->get("type");
         $checkedMessages = json_decode(stripslashes($request->get("checkedMessages")));
         $messageService = $this->get('UserService');
-        foreach($checkedMessages as $cm) {
+        foreach ($checkedMessages as $cm) {
             $messageService->setUserDeleteStatus($cm, 1, $type);
         }
-        echo "success"; exit;
+        echo "success";
+        exit;
     }
-  
+
     /**
-    * Function to view message
-    */
+     * Function to view message
+     */
     public function viewMessageAction($mid)
     {
         $messageService = $this->get('UserService');
@@ -184,28 +191,47 @@ class MessageController extends Controller
         $unreadcount = $messageService->getUnreadMessagesCount($userid);
         $messageService->setReadViewStatus($mid);
         $message = $messageService->getMessage($mid);
-		$content = nl2br($message->getMessage());
-        return $this->render('GqAusUserBundle:Message:message.html.twig', array('unreadcount' => $unreadcount, "message" => $message, 'content' => $content));
+        $msgUser = $message->getSent()->getId();
+        if ($userid == $msgUser) {
+            $userName= $message->getInbox()->getUserName(); 
+            $from = " from me";
+        } else {
+            $userName = $message->getSent()->getUserName(); 
+            $from = " to me";
+            
+        }
+        $content = nl2br($message->getMessage());
+        return $this->render(
+                'GqAusUserBundle:Message:message.html.twig',
+                array(
+                    'unreadcount' => $unreadcount,
+                    "message" => $message,
+                    'content' => $content,
+                    'userName' => $userName,
+                    'from' => $from
+                )
+        );
     }
-    
+
     /**
-    * Function to delete messages from tash
-    */
+     * Function to delete messages from tash
+     */
     public function deleteFromTrashAction(Request $request)
     {
         $checkedMessages = json_decode(stripslashes($request->get("checkedMessages")));
         $messageService = $this->get('UserService');
         $userid = $messageService->getCurrentUser()->getId();
-        foreach($checkedMessages as $cm) {
+        foreach ($checkedMessages as $cm) {
             $messageService->setToUserDeleteFromTrash($userid, $cm, 2);
         }
-        echo "success"; exit;
+        echo "success";
+        exit;
     }
-    
-     /**
-    * Function to unread count
-    * return response
-    */
+
+    /**
+     * Function to unread count
+     * return response
+     */
     public function unreadAction(Request $request)
     {
         $messageService = $this->get('UserService');
@@ -213,5 +239,7 @@ class MessageController extends Controller
         $unreadcount = $messageService->getUnreadMessagesCount($userid);
         echo $unreadcount;
         exit;
-    }
+    }   
+    
+
 }
