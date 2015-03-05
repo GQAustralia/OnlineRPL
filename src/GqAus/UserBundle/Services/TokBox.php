745722192f98e12a2cf6af99ee877eb1513ae130
prashanthvanama <prashanthvanama@github.com>
2015-03-05 18:24:09 +0530

    commited conversation functionality with hTML

--- src/GqAus/UserBundle/Services/TokBox.php
+++ src/GqAus/UserBundle/Services/TokBox.php
@@ -18,6 +18,10 @@ class TokBox {
         $this->repository = $em->getRepository('GqAusUserBundle:Room');
     }
 
+    public function getRoom($roomId) {
+        return $this->repository->find($roomId);
+    }
+
     public function updateRoom($id, $user_id) {
         $room = $this->repository->find($id);
         $room->setApplicant($user_id);
@@ -30,13 +34,18 @@ class TokBox {
      *  function to create a room for video communication.
      *  @return int
      */
-    public function createRoom($sessionId, $user_id) {
+    public function createRoom($sessionId, $user_id, $applicantID) {
         $room = new Room();
         $room->setAssessor($user_id);
         $room->setSession($sessionId);
+        $room->setApplicant($applicantID);
         $this->em->persist($room);
         $this->em->flush();
         return $room->getId();
+    }
+
+    public function isRoomExists($assessorId, $applicantID) {
+        return $this->repository->findOneBy(array('assessor' => $assessorId, 'applicant' => $applicantID));
     }
 
 }
