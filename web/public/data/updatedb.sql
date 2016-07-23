afdbe60ab9d9a3b07768e405d9aa93d45b523b6c
unknown <madhavi.garimella@FVTIII659F1D.T03.ads.valuelabs.net>
2016-07-23 13:34:00 +0530

    DB update

--- web/public/data/updatedb.sql
+++ web/public/data/updatedb.sql
@@ -4,3 +4,5 @@ ALTER TABLE `note` ADD `course_id` INT(10) DEFAULT '0' AFTER `unit_id`;
 -- Add replymid field for message Table
 ALTER TABLE `message` ADD `replymid` INT(10) NOT NULL AFTER `reply`;
 
+ALTER TABLE `user_ids` ADD `size` VARCHAR(20) NOT NULL AFTER `created`;
+
