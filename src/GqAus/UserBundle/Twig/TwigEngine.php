96e248402146e5969e4d8100ecd468b1255a6921
Madhavi <madhavi.garimella@FVTIII659F1D.T03.ads.valuelabs.net>
2016-08-02 10:37:35 +0530

    landing popups pages added

---
+++ src/GqAus/UserBundle/Twig/TwigEngine.php
@@ -0,0 +1,23 @@
+<?php
+namespace GqAus\UserBundle\Twig;
+use Symfony\Bundle\TwigBundle\TwigEngine as BaseTwigEngine;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Component\Config\FileLocatorInterface;
+use Detection\MobileDetect;
+
+class TwigEngine extends BaseTwigEngine
+{
+    protected $mobileDetect;
+    public function __construct(\Twig_Environment $environment, TemplateNameParserInterface $parser, FileLocatorInterface $locator, MobileDetect $mobileDetect)
+    {
+        $this->mobileDetect = $mobileDetect;
+        parent::__construct($environment, $parser, $locator);
+    }
+    public function render($name, array $parameters = array())
+    {
+        if ($this->mobileDetect->isMobile()) {
+            $name = preg_replace('~:(?!.*:)~', '/mobile:', $name);
+        }
+        return parent::render($name, $parameters);
+    }
+}
