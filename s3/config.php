988007cbabccc505195d7e23f240a3a3245fda54
venki kancharlapalli <vkancharlapalli@FVTIII660F1L.T03.ads.valuelabs.net>
2016-09-02 20:31:48 +0530

    added config file

---
+++ s3/config.php
@@ -0,0 +1,17 @@
+<?php
+
+define('AWS_KEY', 'AKIAJXKS7MGMDP6Y3MAA');
+define('AWS_SECRET', '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');
+define('BUCKET_NAME', 'onlinerplevidences');
+define('BUCKET_NAME2', 'testingcdn');
+//define('BUCKET_NAME', 'testingcdn');
+//define('PIPELINE_ID', '1471507016405-zf1ti5'); 
+define('PIPELINE_ID', '1472550420769-h9rl7l');
+define('ACL', 'public-read');
+define('VIDEOPRESET', '1351620000001-000061');
+define('AUDIOPRESET', '1351620000001-300040');
+define('QUEUEURL',  'https://sqs.ap-southeast-2.amazonaws.com/187591088561/PipelineQueue');
+define('REGION', 'ap-southeast-2');
+define('ONLINE_RPL_APP_URL', 'http://onlinerpl.stg.valuelabs.com/');
+
+
