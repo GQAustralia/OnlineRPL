eff1963d8b10007692c22e806f63e505e25b8c7d
Kishore babu thalla <kishore.thalla@FVTIII291F1D.T03.ads.valuelabs.net>
2016-09-06 21:09:39 +0530

    Updated changes

--- src/GqAus/UserBundle/Services/UserService.php
+++ src/GqAus/UserBundle/Services/UserService.php
@@ -522,8 +522,8 @@ class UserService
             $facMailSubject = str_replace($subSearch, $subReplace, $this->container->getParameter('mail_disappove_evdience_fac_sub'));
 
             // finding and replacing the variables from message templates
-            $msgSearch = array('#toUserName#', '#courseCode#', '#courseName#', '#unitId#', '#unitName#', '#fromUserName#');
-            $msgReplace = array($userName, $result['courseCode'], $result['courseName'], $result['unit'], $result['unitName'], $facilitatorName);
+            $msgSearch = array('#toUserName#', '#courseCode#', '#courseName#', '#unitId#', '#unitName#', '#fromUserName#', '#applicationUrl#');
+            $msgReplace = array($userName, $result['courseCode'], $result['courseName'], $result['unit'], $result['unitName'], $facilitatorName, $this->container->getParameter('applicationUrl'));
             $facMessageBody = str_replace($msgSearch, $msgReplace, $this->container->getParameter('msg_disappove_evdience_fac_con'));
             $facMailBody = str_replace($msgSearch, $msgReplace, $this->container->getParameter('mail_disappove_evdience_fac_con'));
             if ($result['userRole'] == 'ROLE_ASSESSOR') {
