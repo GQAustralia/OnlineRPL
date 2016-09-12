555de0c5f23bb2aac77cff64eb4568336622de1e
Ashok Kumar <ashok.chelikani@valuelabs.com>
2016-09-12 15:33:46 +0530

    forgot to remove debugger so removed it

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -366,7 +366,6 @@ function centerModals($element) {
     $modals.each( function(i) {
         var $clone = $(this).clone().css('display', 'block').appendTo('body');
 		var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
-		debugger;
 		top = top > 0 ? top : 0;
         $clone.remove();
        // $(this).find('.modal-content').css("margin-top", top);
