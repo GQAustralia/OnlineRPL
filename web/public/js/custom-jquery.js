69f4afcf0a11b1adbfeb8727d4caad60dbae00b1
Kishore babu thalla <kishore.thalla@FVTIII291F1D.T03.ads.valuelabs.net>
2016-09-19 14:05:47 +0530

    OR-339 | Assessor Login: In mobile 'CC' button is clickable twice

--- web/public/js/custom-jquery.js
+++ web/public/js/custom-jquery.js
@@ -2253,6 +2253,7 @@ $("body").on("click", "#submittoassessor,#request-cc", function(){
                                     }
                                     if(courseStatus == '10'){ 
                                         $("body #courseStatus").val(courseStatus);  
+                                        $("body #request-cc").prop("disabled", true ).addClass("disabled");
                                     }
                                     if(courseStatus == '12'){ 
                                         $("body .competency-call").hide( "slow"); 
