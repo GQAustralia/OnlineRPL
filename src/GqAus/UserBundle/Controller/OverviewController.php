41d86e488c65d9611bba11b1803ab8c1526b5ea8
JemRaymundo <jeremuel.raymundo@gqaustralia.edu.au>
2017-01-25 13:07:22 +0800

    Applied Tour Skin

--- src/GqAus/UserBundle/Controller/OverviewController.php
+++ src/GqAus/UserBundle/Controller/OverviewController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class OverviewController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
-        return $this->render('GqAusUserBundle:Overview:index.html.twig', []);
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        if (is_object($user) && count($user) > 0) {
+            return $this->render('GqAusUserBundle:Overview:index.html.twig');
+        }
+
+        return $this->redirect('login');
     }
 }
