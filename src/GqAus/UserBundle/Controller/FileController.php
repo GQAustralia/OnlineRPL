6e9d5b34d45788db505dff11f546461dc09e242b
Kishore Thalla <kishorebabu.thalla@gmail.com>
2017-02-26 01:39:01 +0530

    Account Manager : linked the evidence tab link irrespective with candidate

--- src/GqAus/UserBundle/Controller/FileController.php
+++ src/GqAus/UserBundle/Controller/FileController.php
@@ -24,7 +24,6 @@ class FileController extends Controller
         if (in_array('ROLE_APPLICANT', $userRole)) {
             $userCourses = $user->getCourses();
             $evidences = $evidenceService->currentUser->getEvidences();
-            $evidenceCats = $evidenceService->getEvidenceCats();
         } else {
             $userId = $request->get('userId');
             $toViewUser = $evidenceService->userService->getUserInfo($userId);
@@ -41,6 +40,7 @@ class FileController extends Controller
             
             //$evidences = array();
         }
+        $evidenceCats = $evidenceService->getEvidenceCats();
         $formattedEvd = $evidenceService->formatEvidencesListToDisplay($evidences);
         return $this->render('GqAusUserBundle:File:view.html.twig', array('evidences' => $formattedEvd['formattedEvidences'], 'evdMapping' => $formattedEvd['evdMapping'], 'courses' => $userCourses,'userName' => $userName,'courseCode' => $courseCode,'userId' => $userId, 'evidenceCats' => $evidenceCats));
     }
