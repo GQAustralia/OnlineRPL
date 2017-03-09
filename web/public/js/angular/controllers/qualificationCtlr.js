a67a21991dc0a8b53129e7c00b5a6aa293645dc8
Kishore thalla <kishorebabu.thalla@gmail.com>
2017-03-09 15:20:15 +0530

    Hide the print button fir IOS devices

--- web/public/js/angular/controllers/qualificationCtlr.js
+++ web/public/js/angular/controllers/qualificationCtlr.js
@@ -66,7 +66,7 @@ gqAus.controller('qualificationCtlr', function($rootScope, $scope, $window, _, A
     $scope.selectedUnitObj = [];
     $scope.selfAssessment = {};
     $scope.notes = [];
-//    $scope.role = 'candidate';
+    $scope.operatingSys = '';
 
     $scope.addRemoveUnit = function(unit) {
 
@@ -337,7 +337,8 @@ gqAus.controller('qualificationCtlr', function($rootScope, $scope, $window, _, A
         }).finally(function(){
             FILE_THUMBNAIL.computation_interval();
         });
-        console.log('338')
+        console.log('338');
+        $scope.operatingSys = getMobileOperatingSystem();
     };
 
     $scope.removeUpload = function(index) {
@@ -732,6 +733,22 @@ gqAus.controller('qualificationCtlr', function($rootScope, $scope, $window, _, A
         }, function(error) {
             console.log(error);
         });
+    }
+    
+    var getMobileOperatingSystem = function() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+        return "";
     }
 
 });
